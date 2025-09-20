6758742a7e4212450d708a0022560e335c8be48e
Saumy003 <saumysingh2005@gmail.com>
2025-09-20 16:52:48 +0530

    opertors used in python

---
+++ Operators.py
@@ -0,0 +1,35 @@
+# 1. arithmatic operators
+a = 9
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a / b)
+print(a % b)  #remainder 
+print(a**b)   #a to the power b
+
+# 2. Relational Operators
+a = 20
+b = 50
+
+ print(a == b)  #False
+ print(a != b)  #True
+ print(a < b)   #True
+ print(a > b)   #False
+ print(a <= b)  #True
+ print(a >= b)  #False
+
+ # 3. Assignment Operators
+ num = 10
+ num += 5
+ print(num) # output:15
+
+num = 10
+num *= 5
+ print(num) # output:50
+
+ # 4. Logical Operators
+ print(not False)    #output: True
+ print(not True)     #output: False
+
