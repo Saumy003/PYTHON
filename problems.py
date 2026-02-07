c600205e77e718ffdf3da5c827bc11d289807799
Saumy003 <saumysingh2005@gmail.com>
2026-02-08 00:01:31 +0530

    practice problems in py

---
+++ problems.py
@@ -0,0 +1,13 @@
+# 1. Program to add two numbers.
+num1 = 30 
+num2 = 40
+print("the sum of given two are =" ,num1 + num2)
+
+### another method
+
+num3 = float(input("enter the first number="))
+num4 = float(input("enter the second number="))
+
+sum = num3 + num4
+
+print("the sum of both the number are=" , sum)
