4f734ade06f250f60da889c461ab205a9304e369
Saumy003 <saumysingh2005@gmail.com>
2026-02-09 00:29:23 +0530

    prime number in python

---
+++ prime.py
@@ -0,0 +1,9 @@
+## code to print prime number
+num = int(input("enter the number:"))
+
+for i in range(2,num):
+    if(num % i == 0):
+        print("not prime")
+        break
+else:
+    print("prime")
