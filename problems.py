cd23d58df4a32cb1f722fc812e82e931e4270808
Saumy003 <saumysingh2005@gmail.com>
2026-02-09 00:30:40 +0530

    problems set in python

--- problems.py
+++ problems.py
@@ -11,3 +11,13 @@ num4 = float(input("enter the second number="))
 sum = num3 + num4
 
 print("the sum of both the number are=" , sum)
+
+
+## code to print prime number
+num = int(input("enter the number:"))
+
+for i in range(2,num):
+    if(num % i == 0):
+        print("not prime")
+    else:
+        print("it is prime")
