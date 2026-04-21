8ac09a9110a90e66b9d945a21e0618b249dbea64
Saumy003 <saumysingh2005@gmail.com>
2026-04-21 22:49:16 +0530

    file handing in python

---
+++ files_handling/basics.py
@@ -0,0 +1,15 @@
+# case1 => if the file is not present
+
+f = open("sample_1.txt", "w")
+f.write("Hello ! I am learning File Handling")
+f.close()
+
+# case2 => if the file is already present
+
+file_2 = open("D:\PYTHON\FILE HANDLING\sample_2.txt", "w")
+file_2.write("Jai Rajputana")
+file_2.close()
+
+# case 3 => 
+
+
