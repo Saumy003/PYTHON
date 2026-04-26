00f9c816bb478b483bfa2e574939bbece89b53cb
Saumy003 <saumysingh2005@gmail.com>
2026-04-26 22:35:17 +0530

    basics of file handling

--- files_handling/basics.py
+++ files_handling/basics.py
@@ -10,6 +10,40 @@ file_2 = open("D:\PYTHON\FILE HANDLING\sample_2.txt", "w")
 file_2.write("Jai Rajputana")
 file_2.close()
 
-# case 3 => 
+# problem with w mode => it replace previous content in file
+# introducing append mode
+file = open("D:\PYTHON\\files_handling\sample_2.txt" , "a")
+file.write("\nJai Maa Bhawati")
+file.close()
 
 
+# write lines
+L = ["hii\n", "hello\n", "kaise ho\n", "main badhiya tum bataoo\n"]
+f = open("D:\PYTHON\\files_handling\sample_1.txt", "w")
+f.writelines(L)
+f.close()
+
+# reading from file 
+# using read()
+f = open("D:\PYTHON\\files_handling\sample_2.txt", "r")
+read_data = f.read()
+print(read_data)
+f.close
+
+# readline() --> to read line by line
+f = open("D:\PYTHON\\files_handling\sample_1.txt", "r")
+print(f.readline(), end="")
+print(f.readline(), end="")
+f.close()
+
+# read entire using readline  {nice concept}
+
+f = open("D:\PYTHON\\files_handling\sample_2.txt", "r")
+while True:
+    data = f.readline()
+
+    if data == "":
+        break
+    else:
+        print(data, end="")
+f.close()
