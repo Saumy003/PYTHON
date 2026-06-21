d96182b78869c0d47d5d67dec4c054b7a95286ad
Saumy003 <saumysingh2005@gmail.com>
2026-06-21 22:23:13 +0530

    DataTypes, Operators, Type Conversion, Basic Qustions in Python

---
+++ Basic Python/FirstProgram.py
@@ -0,0 +1,3 @@
+print("Hello World ." , "My name is Saumy Kumar." ," My age is 20")
+print("532005")
+print(5*3)
---
+++ Basic Python/TypeConversion.py
@@ -0,0 +1,11 @@
+#type Conversion
+a = 2   #int value
+b = 4.25    #float value
+print(a+b)  #output=>6.25 i.e. float
+
+#type casting  :- convert one datatype to other datatype
+
+
+a = float("2")  #string se float me type cast ho gaya
+b = 5.25        #float
+print(a+b)      #output: 7.25  i.e. float
